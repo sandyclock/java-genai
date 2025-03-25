dbef8b4fcb8f468cf5c4024e4ffe74c7a2380cc5
Jaycee Li <jayceeli@google.com>
2025-03-25 14:10:41 -0700

    fix!: Only expose `toJson` and `fromJson` as public methods in JsonSerializable
    PiperOrigin-RevId: 740480028

--- src/main/java/com/google/genai/JsonSerializable.java
+++ src/main/java/com/google/genai/JsonSerializable.java
@@ -22,11 +22,12 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.google.api.core.InternalApi;
 
 /** A class that can be serialized to JSON and deserialized from JSON. */
 public abstract class JsonSerializable {
 
-  protected static final ObjectMapper objectMapper = new ObjectMapper();
+  static final ObjectMapper objectMapper = new ObjectMapper();
 
   static {
     objectMapper.setSerializationInclusion(JsonInclude.Include.NON_ABSENT);
@@ -34,8 +35,13 @@ public abstract class JsonSerializable {
     objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
   }
 
+  /** Serializes the instance to a Json string. */
+  public String toJson() {
+    return toJsonString(this);
+  }
+
   /** Serializes an object to a Json string. */
-  protected static String toJsonString(Object object) {
+  static String toJsonString(Object object) {
     try {
       return objectMapper.writeValueAsString(object);
     } catch (JsonProcessingException e) {
@@ -43,18 +49,15 @@ public abstract class JsonSerializable {
     }
   }
 
-  /** Serializes the instance to a Json string. */
-  public String toJson() {
-    return toJsonString(this);
-  }
-
   /** Serializes an object to a JsonNode. */
-  protected static JsonNode toJsonNode(Object object) {
+  static JsonNode toJsonNode(Object object) {
     return objectMapper.valueToTree(object);
   }
 
-  /** Deserializes a Json string to an object of the given type. */
-  public static <T extends JsonSerializable> T fromJsonString(String jsonString, Class<T> clazz) {
+  /** Deserializes a Json string to an object of the given type. This is for internal use only. */
+  @InternalApi
+  protected static <T extends JsonSerializable> T fromJsonString(
+      String jsonString, Class<T> clazz) {
     try {
       return objectMapper.readValue(jsonString, clazz);
     } catch (JsonProcessingException e) {
@@ -63,7 +66,7 @@ public abstract class JsonSerializable {
   }
 
   /** Deserializes a JsonNode to an object of the given type. */
-  public static <T extends JsonSerializable> T fromJsonNode(JsonNode jsonNode, Class<T> clazz) {
+  static <T extends JsonSerializable> T fromJsonNode(JsonNode jsonNode, Class<T> clazz) {
     try {
       return objectMapper.treeToValue(jsonNode, clazz);
     } catch (JsonProcessingException e) {
